61111781c3b34897ada855df7a60827fd05d56a0
kemboicheru <kemboicheru@gmail.com>
2018-12-03 10:51:12 +0300

    Remove code

--- helpline/views.py
+++ helpline/views.py
@@ -759,9 +759,8 @@ def reports(request, report, casetype='Call'):
     """Report processing and rendering"""
     default_service = Service.objects.all().first()
     default_service_xform = default_service.walkin_xform
-    default_service_auth_token = '7331a310c46884d2643ca9805aaf0d420ebfc831' # default_service_xform.user.auth_token
+    default_service_auth_token = default_service_xform.user.auth_token
     current_site = get_current_site(request)
-    default_service_xform.pk = 39
 
     # Graph data
     headers = {
